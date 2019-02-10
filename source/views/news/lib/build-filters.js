b5501d78c955bfb981e61684c13b9f29825ccd6a
Drew Volz <volz.drew@gmail.com>
2019-02-10 11:34:04 -0800

    We'll move entity decoding to the server

--- source/views/news/lib/build-filters.js
+++ source/views/news/lib/build-filters.js
@@ -4,14 +4,13 @@ import type {StoryType} from './types'
 import flatten from 'lodash/flatten'
 import uniq from 'lodash/uniq'
 import type {FilterType} from '@frogpond/filter'
-import {entities} from '@frogpond/html-lib'
 
 export function buildFilters(newsItems: StoryType[]): FilterType[] {
 	// Format the items for the categories filter
 	const categories = flatten(newsItems.map(item => item.categories))
 	const categoryLabels = uniq(categories)
 	const allCategories = categoryLabels.map(label => ({
-		title: entities.decode(label),
+		title: label,
 	}))
 
 	return [
