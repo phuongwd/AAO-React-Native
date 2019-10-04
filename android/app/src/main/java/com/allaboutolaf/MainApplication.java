aacbb6cb583376af763da1fcdc6b294ec5ba71c8
Drew Volz <volz.drew@gmail.com>
2019-10-03 19:41:55 -0700

    Upgrade react native to v0.61.2

--- android/app/src/main/java/com/allaboutolaf/MainApplication.java
+++ android/app/src/main/java/com/allaboutolaf/MainApplication.java
@@ -2,13 +2,14 @@ package com.allaboutolaf;
 
 import android.app.Application;
 import android.net.http.HttpResponseCache;
-import android.util.Log;
+import android.content.Context;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
 
 import java.io.File;
 import java.io.IOException;
